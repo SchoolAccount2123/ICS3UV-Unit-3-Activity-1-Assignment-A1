4d18aec3ac4b8295ef816e4ee134f0ad10b908b3
Connor-Wilkins <159037610+SchoolAccount2123@users.noreply.github.com>
2025-11-17 20:22:30 +0000

    finnished code

---
+++ main.ts
@@ -0,0 +1,14 @@
+//this code will calculate the area of a circle with a radius of 5.6 cm
+
+// making the variables
+const PI: number = 3.14;
+const radius: number = 5.6;
+
+//calculation
+
+const area: number = PI * radius * radius;
+
+//display the results
+
+console.log("The Radius is:", radius, "cm");
+console.log("The area of the circle is:", area.toFixed(2), "square cm")
